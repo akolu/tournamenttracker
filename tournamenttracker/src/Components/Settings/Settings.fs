2f627b8bde6112e444a445385f777f57cb3c0940
Aku Kolu <aku.kolu@mehilainen.fi>
2022-04-26 00:44:31 +0300

    rounds & players in settings component state

--- tournamenttracker/src/Components/Settings/Settings.fs
+++ tournamenttracker/src/Components/Settings/Settings.fs
@@ -2,20 +2,55 @@ module App.Components.Settings
 
 open Feliz
 open Feliz.Bulma
+open Fable.FontAwesome
 
 Fable.Core.JsInterop.importSideEffects "./Settings.scss"
 
 [<ReactComponent>]
-let private IconButton (icon: ReactElement) =
+let private IconButton (icon: Fa.IconOption, fn: Browser.Types.MouseEvent -> unit) =
     Bulma.button.button [
         button.isRounded
         button.isSmall
+        prop.onClick fn
         prop.className "Settings__button__IconButton"
-        prop.children [ Bulma.icon icon ]
+        prop.children [
+            Bulma.icon (Fa.i [ icon ] [])
+        ]
     ]
+
+type private IconButtonAction =
+    | IncRounds
+    | DecRounds
+    | IncPlayers
+    | DecPlayers
 
 [<ReactComponent>]
 let Settings () =
+
+    let (rounds, setRounds) = React.useState (1)
+    let (players, setPlayers) = React.useState [ ("", 0) ]
+
+    let iconClick action =
+        (fun _ ->
+            match action with
+            | DecRounds when rounds > 1 -> setRounds (rounds - 1)
+            | IncRounds when rounds < 9 -> setRounds (rounds + 1)
+            | IncPlayers -> setPlayers (players |> List.append [ ("", 0) ])
+            | DecPlayers when players.Length > 1 -> setPlayers (players.[.. players.Length - 2])
+            | DecPlayers when players.Length = 1 -> setPlayers ([ "", 0 ])
+            | _ -> ())
+
+    let playerNameChanged index name =
+        setPlayers (
+            players
+            |> List.mapi (fun i p -> if i = index then (name, snd p) else p)
+        )
+
+    let printPlayers (event: Browser.Types.KeyboardEvent) =
+        if event.key = "Enter" then
+            players
+            |> Seq.iter (fun p -> System.Console.WriteLine(fst p + ": " + (snd p).ToString()))
+
     Html.div [
         prop.className "Settings__div__root"
         prop.children [
@@ -35,7 +70,7 @@ let Settings () =
                                             Bulma.input.text [
                                                 input.isRounded
                                                 input.isSmall
-                                                prop.value "Tournament name"
+                                                prop.defaultValue "Tournament name"
                                             ]
                                         ]
                                     ]
@@ -44,11 +79,25 @@ let Settings () =
                                         prop.children [
                                             Bulma.label "Rounds"
                                             Html.div [
-                                                prop.className "Settings__div__rounds"
+                                                prop.className "Settings__NumberSelector--wrapper"
                                                 prop.children [
-                                                    IconButton(Html.i [ prop.className "fa fa-minus" ])
-                                                    Html.div "1"
-                                                    IconButton(Html.i [ prop.className "fa fa-plus" ])
+                                                    IconButton(Fa.Solid.Minus, iconClick DecRounds)
+                                                    Html.div rounds
+                                                    IconButton(Fa.Solid.Plus, iconClick IncRounds)
+                                                ]
+                                            ]
+                                        ]
+                                    ]
+                                    Html.div [
+                                        prop.className "Settings__div__field"
+                                        prop.children [
+                                            Bulma.label "Players"
+                                            Html.div [
+                                                prop.className "Settings__NumberSelector--wrapper"
+                                                prop.children [
+                                                    IconButton(Fa.Solid.Minus, iconClick DecPlayers)
+                                                    Html.div players.Length
+                                                    IconButton(Fa.Solid.Plus, iconClick IncPlayers)
                                                 ]
                                             ]
                                         ]
@@ -59,17 +108,27 @@ let Settings () =
                                 column.isOneThird
                                 prop.children [
                                     Bulma.Divider.divider "Players"
-                                    Html.div [
-                                        prop.className "Settings__Players--inputwrapper"
-                                        prop.children [
-                                            Bulma.input.text [
-                                                input.isRounded
-                                                input.isSmall
-                                                prop.placeholder "Enter player name"
-                                            ]
-                                            IconButton(Html.i [ prop.className "fa fa-plus" ])
-                                        ]
-                                    ]
+                                    Html.div (
+                                        players
+                                        |> List.mapi (fun i p ->
+                                            Html.div [
+                                                prop.className "Settings__Players--inputwrapper"
+                                                prop.children [
+                                                    Bulma.input.text [
+                                                        input.isRounded
+                                                        input.isSmall
+                                                        prop.onKeyDown printPlayers
+                                                        prop.onChange (fun (ev: string) -> playerNameChanged i ev)
+                                                        prop.placeholder "Player name"
+                                                    ]
+                                                    Bulma.input.text [
+                                                        input.isRounded
+                                                        input.isSmall
+                                                        prop.placeholder "Rating"
+                                                    ]
+                                                ]
+                                            ])
+                                    )
                                 ]
                             ]
                             Bulma.column [
